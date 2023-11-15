e5efbbbed338328430f2377b690c1763f324a8e9
Skyler <sanderson2737@hotmail.com>
2023-11-15 13:11:00 -0500

    v1.0.0

---
+++ C#/TicTacToe/TicTacToe/Game.cs
@@ -0,0 +1,107 @@
+﻿
+using System;
+
+namespace TicTacToe
+{
+    internal class Game
+    {
+        char[,] grid = new char[3, 3];
+        public Game()
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    grid[x, y] = '.';
+                }
+            }
+        }
+
+        public bool Ended()
+        {
+            // Check for win conditions
+            for (int i = 0; i < 3; i++)
+            {
+                // Check rows and columns
+                if ((grid[i, 0] == grid[i, 1] && grid[i, 1] == grid[i, 2] && grid[i, 0] != '.') ||
+                    (grid[0, i] == grid[1, i] && grid[1, i] == grid[2, i] && grid[0, i] != '.'))
+                {
+                    return true; // Win condition met
+                }
+            }
+
+            // Check diagonals
+            if ((grid[0, 0] == grid[1, 1] && grid[1, 1] == grid[2, 2] && grid[0, 0] != '.') ||
+                (grid[0, 2] == grid[1, 1] && grid[1, 1] == grid[2, 0] && grid[0, 2] != '.'))
+            {
+                return true; // Win condition met
+            }
+
+            // Check for draw condition (no empty cells)
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    if (grid[x, y] == '.')
+                    {
+                        return false; // Empty cell found, game continues
+                    }
+                }
+            }
+
+            // If no win and no empty cells, it's a draw
+            return true;
+        }
+
+
+        public class BadMoveException : Exception
+        {
+            public BadMoveException(string message) : base(message) { }
+        }
+
+
+        internal void Move(int position, char player)
+        {
+            if (position < 0 || position > 8)
+                throw new ArgumentException("Position must be in the range 0 to 8.");
+
+            if (grid[position % 3, position / 3] != '.')
+                throw new BadMoveException("The selected cell is already occupied.");
+
+            grid[position % 3, position / 3] = player;
+        }
+
+        public void Print()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Key");
+            int cell = 0;
+            for (int y = 0; y < 3; y++)
+            {
+                if (y >= 1) Console.WriteLine("---+---+---");
+                for (int x = 0; x < 3; x++)
+                {
+                    if (x >= 1) Console.Write("|");
+                    Console.Write(" {0} ", cell++);
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine();
+            for (int y = 0; y < 3; y++)
+            {
+                if (y >= 1) Console.WriteLine("---+---+---");
+                for (int x = 0; x < 3; x++)
+                {
+                    if (x >= 1) Console.Write("|");
+                    Console.Write(" {0} ", grid[x, y]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
---
+++ C#/TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+using static TicTacToe.Game;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool playAgain;
+            do
+            {
+                var game = new Game();
+                var player = 'X';
+                playAgain = false;
+
+                while (!game.Ended())
+                {
+                    game.Print();
+                    bool validMove = false;
+
+                    while (!validMove)
+                    {
+                        Console.Write("Enter your move: ");
+                        string input = Console.ReadLine();
+                        int position;
+
+                        if (int.TryParse(input, out position))
+                        {
+                            try
+                            {
+                                game.Move(position, player);
+                                validMove = true; // Move was successful, exit the loop
+                            }
+                            catch (ArgumentException ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                            catch (BadMoveException ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Unknown Error occurred.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. Please enter a number between 0 and 8.");
+                        }
+                    }
+
+                    // Check if the game has ended after the move
+                    if (game.Ended())
+                    {
+                        game.Print(); // Print the final state of the game
+                        break; // Exit the game loop
+                    }
+
+                    // Switch players
+                    player = (player == 'X') ? 'O' : 'X';
+                }
+
+                Console.Write("Play again? (y/n): ");
+                var response = Console.ReadLine();
+                playAgain = response.Equals("y", StringComparison.OrdinalIgnoreCase);
+            } while (playAgain);
+        }
+    }
+
+
+}
