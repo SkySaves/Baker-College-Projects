9cc8397eba8a336770cd4809eee7116b6c852636
Sky <sanderson2737@hotmail.com>
2025-02-12 07:37:24 -0500

    v0.0.5

--- SeniorProject/obj/Debug/net8.0-windows/SeniorProject.AssemblyInfo.vb
+++ SeniorProject/obj/Debug/net8.0-windows/SeniorProject.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("SeniorProject"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+429c1bbd98bae31b7a976ceb47d50e2066aa6591"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("SeniorProject"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("SeniorProject"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
